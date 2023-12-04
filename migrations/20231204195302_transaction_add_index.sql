1a1a0875cb74ae6c8f136ecabf02d6d4870cc737
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-12-04 12:13:04 -0800

    feat(transaction): add index on item_id

---
+++ migrations/20231204195302_transaction_add_index.sql
@@ -0,0 +1,2 @@
+-- Create index "transactionitems_littleroosteritemid_index" to table: "transactionitems"
+CREATE INDEX "transactionitems_littleroosteritemid_index" ON "public"."transactionitems" ("littleroosteritemid");
