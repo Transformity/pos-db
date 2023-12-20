99f3befa04a82609f5d446293d015dd071a16006
Jay Patel <36803168+jay-babu@users.noreply.github.com>
2023-12-20 02:18:12 -0500

    fix: work with prod

--- migrations/20231205221516_add_size_unit.sql
+++ migrations/20231205221516_add_size_unit.sql
@@ -1,17 +1,1 @@
--- Modify "item" table
-ALTER TABLE "public"."item" ALTER COLUMN "size1" TYPE numeric using "size1"::numeric, ADD COLUMN "size_unit" text NULL;
-
-BEGIN;
-
-update item set size1 = (
-    case
-        when size1 >= 1000 then size1 / 1000.0
-        else size1
-        end
-    ), size_unit = (
-    case
-        when size1 >= 1000 then 'L'
-        else 'ML'
-        end
-    );
-END;
+SELECT 1;
